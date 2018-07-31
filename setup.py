72bcee9f54fbb87ee3c55085c1b615b69a03e50a
Daniel Grossmann-Kavanagh <kavanaghdaniel@gmail.com>
2018-07-30 20:52:52 -0700

    add setup.py classifiers

--- setup.py
+++ setup.py
@@ -22,4 +22,14 @@ setup(
     py_modules=['werkzeug_raw'],
     install_requires=['werkzeug', 'six'],
     tests_require=['nose', 'coverage'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
